3687558e2ab276e29bcebc2bd488984bd8e11a9a
Peter Koenen <mail@peterkoenen.nl>
2025-03-15 15:42:15 +0100

    Added Microsoft login
    It's now possible to use the Firebase Microsoft AD identity provider in the app

--- README.md
+++ README.md
@@ -4,3 +4,84 @@
 Open source BPMN collaboration tool
 
 Live demo: https://bpmn.creatoratnight.com/
+
+## Setup
+
+### Prerequisites
+
+- Node.js (v14 or higher)
+- npm (v6 or higher)
+
+### Installation
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/your-username/bpmn-modeler.git
+    cd bpmn-modeler
+    ```
+
+2. Install the dependencies:
+
+    ```
+    npm install
+    ```
+
+### Firebase Setup
+
+1. Go to the Firebase Console.
+2. Create a new project or select an existing project.
+3. Enable the "Realtim Database" for you Firebase project:
+   - Setup Rules for the Realtime Database
+4. Click on the gear icon next to "Project Overview" and select "Project settings".
+5. In the "General" tab, scroll down to "Your apps" and click on the "Web" icon to create a new web app.
+6. Register your app with a nickname and click "Register app".
+7. Create a copy of `src/config/.example.firebase.js` and rename it to `.firebase.js`.
+8. Copy the Firebase configuration object from the Firebase Console paste it into `.firebase.js`.
+9. Enable Authentication in Firebase:
+   - Go to the "Authentication" section in the Firebase Console.
+   - Click on the "Sign-in method" tab.
+   - Enable the "Google" sign-in provider and configure it.
+
+### Microsoft Identity Provider Setup (optional)
+
+1. Go to the Azure Portal.
+2. Register a new application:
+   - Go to "Azure Active Directory" > "App registrations" > "New registration".
+   - Enter a name for your application.
+   - Select what types of accounts you want to give access
+   - Select "Web" as the platform for the redirect URI
+   - Set the "Redirect URI" to `https://{firebase-app-id}.firebaseapp.com/__/auth/handler`.
+   - Click "Register".
+3. Enable the Microsoft sign-in provider in Firebase:
+   - Go to the "Authentication" section in the Firebase Console.
+   - Click on the "Sign-in method" tab.
+   - Enable the "Microsoft" sign-in provider
+   - Copy the "Application (client) ID" from Azure to the Firebase provider config
+   - Copy the "Client secret" (Manage > Certificates & secrets) from Azure to the Firebase provider config
+
+### Configuration
+
+1. Change the settings in `src/config/config.js` to your likings:
+   - Set `enableGoogleSignIn` to `true` if you have setup the Google identity provider in Firebase
+   - Set `enableMicrosoftSignIn` to `true` if you have setup the Microsoft AD identity provider in Firebase
+
+### Running the Application
+
+1. Start the development server:
+   - `npm run dev`
+2. Open your browser and navigate to http://localhost:5173.
+
+### Usage
+
+1. Sign in with Google or Microsoft to access your projects.
+2. Create, edit, and save BPMN and DMN models.
+3. Collaborate with others in real-time.
+
+### Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request.
+
+### License
+
+This project is licensed under the MIT License.
--- src/services/user.service.tsx
+++ src/services/user.service.tsx
@@ -1,12 +1,15 @@
-import {child, get, getDatabase, ref, set} from "firebase/database";
-import {GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
-import {auth} from '../config/.firebase.js';
+import { child, get, getDatabase, ref, set, query, orderByChild, equalTo } from "firebase/database";
+import { GoogleAuthProvider, signInWithPopup, signOut, fetchSignInMethodsForEmail, linkWithCredential, OAuthProvider, EmailAuthProvider } from "firebase/auth";
+import { auth, microsoftProvider } from '../config/.firebase.js';
+import axios from 'axios';
+import { encode } from 'base64-arraybuffer';
 
 export const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
         const result = await signInWithPopup(auth, provider);
         const user = result.user;
+        console.log('Successfully signed in with Google: ', user);
 
         // Get reference to the database
         const db = getDatabase();
@@ -39,7 +42,23 @@ export const signInWithGoogle = async () => {
         });
 
     } catch (error) {
-        console.error('Error during Google sign-in: ', error);
+        if (error.code === 'auth/account-exists-with-different-credential') {
+            const email = error.customData.email;
+            const pendingCred = GoogleAuthProvider.credentialFromError(error);
+            const microsoftProvider = new OAuthProvider('microsoft.com');
+                signInWithPopup(auth, microsoftProvider).then((microsoftResult) => {
+                    // Link the Google credential to the existing user
+                    linkWithCredential(microsoftResult.user, pendingCred).then(() => {
+                        console.log('Google account linked to existing user.');
+                    }).catch((linkError) => {
+                        console.error('Error linking Google account: ', linkError);
+                    });
+                }).catch((microsoftSignInError) => {
+                    console.error('Error signing in with Microsoft: ', microsoftSignInError);
+                });
+        } else {
+            console.error('Error during Google sign-in: ', error);
+        }
     }
 };
 
@@ -49,5 +68,74 @@ export const logout = async () => {
         console.log('User signed out');
     } catch (error) {
         console.error('Error signing out: ', error);
+    }
+};
+
+export const signInWithMicrosoft = async () => {
+    try {
+        const result = await signInWithPopup(auth, microsoftProvider);
+        const user = result.user;
+        console.log('Successfully signed in with Microsoft: ', user);
+
+        // Fetch the user's avatar from Microsoft Graph API
+        const accessToken = result._tokenResponse.oauthAccessToken;
+        const graphResponse = await axios.get('https://graph.microsoft.com/v1.0/me/photo/$value', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+            responseType: 'arraybuffer'
+        });
+
+        const userAvatar = `data:image/jpeg;base64,${encode(graphResponse.data)}`;
+        console.log('User avatar fetched from Microsoft Graph API.', userAvatar);
+
+        // Get reference to the database
+        const db = getDatabase();
+        const userRef = ref(db, 'users/' + user.uid);
+
+        // Check if the user already exists in the database
+        get(child(userRef, '/')).then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log('User already exists in the database.');
+                // Update last login or other relevant fields
+                set(userRef, {
+                    ...snapshot.val(),
+                    email: user.email,
+                    displayName: user.displayName,
+                    imageUrl: userAvatar,
+                    lastLogin: new Date().toISOString()
+                });
+            } else {
+                console.log('New user added to the database.');
+                // Add new user data
+                set(userRef, {
+                    email: user.email,
+                    displayName: user.displayName,
+                    imageUrl: userAvatar,
+                    createdAt: new Date().toISOString()
+                });
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    } catch (error) {
+        if (error.code === 'auth/account-exists-with-different-credential') {
+            const email = error.customData.email;
+            const pendingCred = OAuthProvider.credentialFromError(error);
+            const googleProvider = new GoogleAuthProvider();
+                    signInWithPopup(auth, googleProvider).then((googleResult) => {
+                        // Link the Microsoft credential to the existing user
+                        linkWithCredential(googleResult.user, pendingCred).then(() => {
+                            console.log('Microsoft account linked to existing user.');
+                        }).catch((linkError) => {
+                            console.error('Error linking Microsoft account: ', linkError);
+                        });
+                    }).catch((googleSignInError) => {
+                        console.error('Error signing in with Google: ', googleSignInError);
+                    });
+        } else {
+            console.error('Error during Microsoft sign-in: ', error);
+        }
     }
 };
